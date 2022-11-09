6659e324a063c845f345f21b9bb0a7ec738c05c0
elsieliuliu <elsieliu.uw@gmail.com>
2022-11-09 14:10:37 -0800

    Initial commit

---
+++ MyToDo/MyToDo/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyToDo.Models
+{
+    public class ToDo
+    {
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string FileName { get; set; }
+
+    }
+}
---
+++ MyToDo/MyToDo/Views/ToDoPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+namespace MyToDo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDo : ContentPage
+    {
+        public ToDo()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSaveButton_Clicked(object sender, EventArgs e)
+        {
+            var filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                $"{Path.GetRandomFileName()}.notes.txt");
+            File.WriteAllText(filename,ToDoText.Text);
+ 
+        }
+
+        private void OnDeleteButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
